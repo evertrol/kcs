04c724a972aaca84a1263fb49d100a3767c2784c
Evert Rol <evert.rol@gmail.com>
2020-01-31 17:10:47 +0100

    Add the ksc.config subpackage

---
+++ ksc/config/__init__.py
@@ -0,0 +1,10 @@
+SCENARIO_HISTORICAL = -1
+
+AREAS = {
+    'rhinebasin': {'w': 6, 'e': 9, 'n': 52, 's': 47},
+    'nl': dict(w=3.3, e=7.1, s=50.0, n=53.6),
+    'nlbox': dict(w=4.5, e=8, s=50.5, n=53),
+    'weurbox': dict(w=4, e=14, s=47, n=53),
+    'global': None,
+    'nlpoint': dict(latitude=51.25, longitude=6.25),
+}
