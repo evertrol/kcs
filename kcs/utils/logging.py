46f9ca4d5ade90ba83b3a858385d5cacba9359d8
Evert Rol <evert.rol@gmail.com>
2020-02-28 14:58:10 +0100

    Prevent an index error when setting the logging level

--- kcs/utils/logging.py
+++ kcs/utils/logging.py
@@ -3,7 +3,7 @@ import logging
 
 def setup(verbosity=0, logname='kcs'):
     levels = [logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG]
-    level = levels[max(0, min(verbosity, len(levels)))]
+    level = levels[max(0, min(verbosity, len(levels)-1))]
     handler = logging.StreamHandler()
     handler.setLevel(level)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',
