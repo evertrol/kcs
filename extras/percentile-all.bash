eeda339ac512ba2fc8ea178ee24760ad394c104c
Evert Rol <evert.rol@gmail.com>
2020-03-11 16:21:01 +0100

    Improve helper bash script

--- extras/percentile-all.bash
+++ extras/percentile-all.bash
@@ -1,20 +1,34 @@
 #! /bin/bash
 
+# This uses the `kcs.change_perc.runall` module, which is a small
+# utility that uses a steering table as input to deduce the correct
+# periods for calculating the statistics for the EC-EARTH data.
+
+# It will also creates plots of the CMIP distribution with the
+# individual EC-EARTH runs overplotted. Output file names are
+# generated automatically, following the pattern
+# "{var}_{epoch}_{season}.{ext}".
+
+
 for epoch in 2050 2085
 do
-        fname="steering${epoch}.csv"
+		# Create a separate steering table for this epoch from the main steering table file
+		fname="steering${epoch}.csv"
+		head -n1 steering.csv > $fname
+		grep ",${epoch}," steering.csv >> $fname
+
         for var in tas pr
         do
                 listname1="@cmip5-${var}-nlpoint-averaged.list"
                 listname2="@ecearth-${var}-nlpoint-averaged.list"
-                for season in djf jja
+                for season in djf jja mam son
                 do
                         echo "python -m kcs.change_perc.runall $listname1 --season $season  --steering $fname --runs $listname2 --relative pr --no-matching -v --plottype pdf --write-csv  &"
                         python -m kcs.change_perc.runall $listname1 --season $season  --steering $fname --runs $listname2 --relative pr --no-matching -v --plottype pdf --write-csv  &
                 done
+				# Run four processes at a time.
+				wait
         done
-		# Run four processes at a time, if there are only four cores.
-		#wait
 done
 # Finish the bash script only when all child processes have finished.
 wait
