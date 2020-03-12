c72fb8b40f489122b7aed5eb703d1ab3ad4a8cca
Evert Rol <evert.rol@gmail.com>
2020-03-12 10:40:26 +0100

    Add option to print configuration for just a section

--- kcs/config/__main__.py
+++ kcs/config/__main__.py
@@ -1,8 +1,23 @@
+import sys
 from .default import config_toml_string
 
 
+def main():
+    try:
+        section = sys.argv[1]
+    except IndexError:
+        print(config_toml_string.strip())
+        return
+
+    found = False
+    for line in config_toml_string.split('\n'):
+        if line == f"[{section}]":
+            found = True
+        elif line.startswith('[') and not line.startswith(f"[{section}"):  # new section
+            found = False
+        if found:
+            print(line)
+
+
 if __name__ == '__main__':
-    print(config_toml_string.strip())
-    from pprint import pprint
-    from . import default_config
-    pprint(default_config)
+    main()
