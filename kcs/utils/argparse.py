a2aa2480426dae6b8c92a8a8c96fea23c1a47142
Evert Rol <evert.rol@gmail.com>
2020-03-12 10:42:47 +0100

    Add a --config/-C option to the default kcs parser

--- kcs/utils/argparse.py
+++ kcs/utils/argparse.py
@@ -4,4 +4,5 @@ import argparse
 parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument('-v', '--verbosity', action='count',
                     default=0, help="Verbosity level")
+parser.add_argument('-C', '--config', help="Specify configuration file.")
 parser.add_argument('--nproc', type=int, default=1)
