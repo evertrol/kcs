cb03575894a59b140eab17e83038f73915d33c9d
Evert Rol <evert.rol@gmail.com>
2020-03-19 15:01:37 +0100

    Remove unused modules

--- kcs/types.py
+++
@@ -1,7 +0,0 @@
-from collections import namedtuple
-
-
-Data = namedtuple('Data', ['path', 'realization', 'area', 'cube'])
-DataAttrs = namedtuple('DataAttrs', ['path', 'realization', 'area', 'cube',
-                                     'initialization', 'experiment', 'physics',
-                                     'model'])
